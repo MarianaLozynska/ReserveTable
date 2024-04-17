710c27e76c4702b4265b9b84cff62df58d2cc263
Mariana Lozynska <mariana.lozynska@imgarena.com>
2024-04-18 00:47:56 +0100

    feat: layouts and head meta

--- app/reserve/[slug]/page.tsx
+++ app/reserve/[slug]/page.tsx
@@ -1,19 +1,13 @@
-import NavBar from "../../components/NavBar";
 import Header from "./components.tsx/Header";
 import Form from "./components.tsx/Form";
 
 export default function Reserve() {
   return (
-    <div className="bg-gray-100 min-h-screen w-screen text-black">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <NavBar />
-        <div className="border-t h-screen">
-          <div className="py-9 w-3/5 m-auto">
-            <Header />
-            <Form />
-          </div>
-        </div>
-      </main>
+    <div className="border-t h-screen">
+      <div className="py-9 w-3/5 m-auto">
+        <Header />
+        <Form />
+      </div>
     </div>
   );
 }
--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -5,17 +5,11 @@ import Menu from "../components/Menu";
 
 export default function RestaurantMenu() {
   return (
-    <div className="bg-gray-100 min-h-screen w-screen text-black">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        <NavBar />
-        <Header />
-        <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11">
-          <div className="bg-white w-[100%] rounded p-3 shadow">
-            <RestaurantNavBar />
-            <Menu />
-          </div>
-        </div>
-      </main>
-    </div>
+    <>
+      <div className="bg-white w-[100%] rounded p-3 shadow">
+        <RestaurantNavBar />
+        <Menu />
+      </div>
+    </>
   );
 }
