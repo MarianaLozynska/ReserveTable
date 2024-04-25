f216cf1ecd6b913951ed6c9bd0a12f380f023c02
Mariana Lozynska <mariana.lozynska@imgarena.com>
2024-04-25 13:50:14 +0100

    feat: signup endpoint

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,6 +1,10 @@
-import { error } from "console";
+import bcrypt from "bcrypt";
 import { NextApiRequest, NextApiResponse } from "next";
 import validator from "validator";
+import { PrismaClient } from "@prisma/client";
+import * as jose from "jose";
+
+const prisma = new PrismaClient();
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -42,6 +46,35 @@ export default async function handler(
     if (errors.length) {
       return res.status(400).json({ errorMessage: errors[0] });
     }
-    res.status(200).json({ hello: "body" });
+    const userWithEmail = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (userWithEmail) {
+      return res
+        .status(400)
+        .json({ errorMessage: "Email is associated with another account" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await prisma.user.create({
+      data: {
+        first_name: firstName,
+        last_name: lastName,
+        password: hashedPassword,
+        city,
+        email,
+        phone,
+      },
+    });
+    const alg = "HS256";
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+    const token = await new jose.SignJWT({ email: user.email })
+      .setProtectedHeader({
+        alg,
+      })
+      .setExpirationTime("24h")
+      .sign(secret);
+    res.status(200).json({ hello: token });
   }
 }
