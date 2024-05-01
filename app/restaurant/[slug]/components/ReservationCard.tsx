856b3060727dc906a5606bc674ed12079a5a3438
Mariana Lozynska <mariana.lozynska@imgarena.com>
2024-05-01 12:40:30 +0100

    feat: availability system

--- app/restaurant/[slug]/components/ReservationCard.tsx
+++ app/restaurant/[slug]/components/ReservationCard.tsx
@@ -1,22 +1,47 @@
 "use client";
 import { useState } from "react";
-import { partySize, times } from "../../../../data";
+import { partySize as partySizes, times } from "../../../../data";
 import DatePicker from "react-datepicker";
+import useAvailabilities from "../../../../hooks/useAvailabilities";
+import { CircularProgress, Link } from "@mui/material";
+import {
+  Time,
+  convertToDisplayTime,
+} from "../../../../utils/convertToDisplayTime";
 
 export default function ReservationCard({
   openTime,
   closeTime,
+  slug,
 }: {
   openTime: string;
   closeTime: string;
+  slug: string;
 }) {
+  const { data, loading, error, fetchAvailabilities } = useAvailabilities();
   const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
+  const [time, setTime] = useState(openTime);
+  const [partySize, setPartySize] = useState("2");
+  const [day, setDay] = useState(new Date().toISOString().split("T")[0]);
+  console.log({ data });
   const handleChangeDate = (date: Date | null) => {
     if (date) {
+      setDay(date.toISOString().split("T")[0]);
       return setSelectedDate(date);
     }
     return setSelectedDate(null);
   };
+
+  const handleClick = () => {
+    console.log(slug, day, time, partySize);
+    fetchAvailabilities({
+      slug,
+      day,
+      time,
+      partySize,
+    });
+  };
+
   const filterTimeByRestaurantOpenWindow = () => {
     const timesWithnWindow: typeof times = [];
     let isWithinWindow = false;
@@ -35,14 +60,20 @@ export default function ReservationCard({
   };
 
   return (
-    <div className="fixed w-[15%] bg-white rounded p-3 shadow">
+    <div className=" bg-white rounded p-3 shadow">
       <div className="text-center border-b pb-2 font-bold">
         <h4 className="mr-7 text-lg">Make a Reservation</h4>
       </div>
       <div className="my-3 flex flex-col">
         <label htmlFor="">Party size</label>
-        <select name="" className="py-3 border-b font-light bg-white" id="">
-          {partySize.map((size) => (
+        <select
+          name=""
+          className="py-3 border-b font-light bg-white"
+          id=""
+          value={partySize}
+          onChange={(e) => setPartySize(e.target.value)}
+        >
+          {partySizes.map((size) => (
             <option value={size.value}>{size.label}</option>
           ))}
         </select>
@@ -60,7 +91,13 @@ export default function ReservationCard({
         </div>
         <div className="flex flex-col w-[48%]">
           <label htmlFor="">Time</label>
-          <select name="" id="" className="py-3 border-b font-light bg-white">
+          <select
+            name=""
+            id=""
+            className="py-3 border-b font-light bg-white"
+            value={time}
+            onChange={(e) => setTime(e.target.value)}
+          >
             {filterTimeByRestaurantOpenWindow().map((time) => (
               <option value={time.time}>{time.displayTime}</option>
             ))}
@@ -68,10 +105,35 @@ export default function ReservationCard({
         </div>
       </div>
       <div className="mt-5">
-        <button className="bg-red-600 rounded w-full px-4 text-white font-bold h-16">
-          Find a Time
+        <button
+          className="bg-red-600 rounded w-full px-4 text-white font-bold h-16"
+          onClick={handleClick}
+          disabled={loading}
+        >
+          {loading ? <CircularProgress color="inherit" /> : "Find a Time"}
         </button>
       </div>
+      {data && data.length ? (
+        <div className="mt-4">
+          <p className="text-reg text-black">Select a Time</p>
+          <div className="flex flex-wrap m-2">
+            {data.map((time) => {
+              return time.available ? (
+                <Link
+                  href={`/reserve/${slug}?date=${day}T${time.time}&partySize={partySize}`}
+                  className="bg-red-600 cursor-pointer p-2 w-24 text-center text-white mb-3 rounded mr-3"
+                >
+                  <p className="text-sm font-bold text-black">
+                    {convertToDisplayTime(time.time as Time)}
+                  </p>
+                </Link>
+              ) : (
+                <p className="bg-gray-300 p-2 w-24 m-3 mr-3 rounded"></p>
+              );
+            })}
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 }
